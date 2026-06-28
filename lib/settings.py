25ce1daf3c939109e62978018639379cdb457a00
Pietro <peter.patroz@gmail.com>
2026-06-28 09:14:12 +0200

    feat: save bootstrap implementation

--- lib/settings.py
+++ lib/settings.py
@@ -1,11 +1,17 @@
+import xbmc
 import xbmcaddon
 
 
 class Settings:
 
     def __init__(self):
+        xbmc.log("[AIOBridge] Creating Addon()", xbmc.LOGINFO)
         self.addon = xbmcaddon.Addon()
+        xbmc.log("[AIOBridge] Addon created", xbmc.LOGINFO)
 
     @property
     def manifest_url(self):
-        return self.addon.getSettingString("manifest_url")
+        xbmc.log("[AIOBridge] Reading manifest_url", xbmc.LOGINFO)
+        value = self.addon.getSettingString("manifest_url")
+        xbmc.log(f"[AIOBridge] Value={value}", xbmc.LOGINFO)
+        return value
